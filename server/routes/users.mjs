0f86dd63fdf8f1e5e058a80b7810b3e17b4db1c6
davy.tran <davytran1993@gmail.com>
2023-09-19 12:59:42 +0200

    feat(server): add experiences route

--- server/routes/users.mjs
+++ server/routes/users.mjs
@@ -1,14 +1,13 @@
 import express from "express";
-import db from '../db/conn.mjs';
 import { ObjectId } from "mongodb";
+import '../db/conn.mjs';
 import User from '../models/user.model.mjs';
 
 const router = express.Router();
 
 // Get a list of 50 users
-router.get("/", async (req, res) => {
-    let collection = await db.collection("users");
-    let results = await collection.find({})
+router.get("/", async (_req, res) => {
+    let results = await User.collection.find({})
         .limit(50)
         .toArray();
     res.send(results).status(200);
@@ -24,7 +23,7 @@ router.post("/", async (req, res) => {
 // Get user by id
 router.get("/:id", async (req, res) => {
     let query = { _id: ObjectId(req.params.id) };
-    let result = await User.collection.findOne(query);
+    let result = await User.findOne(query);
     if (!result) {
         res.send("Not found!").status(404);
     } else {
@@ -36,7 +35,7 @@ router.get("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
     const query = { _id: ObjectId(req.params.id) };
 
-    let result = await User.collection.deleteOne(query);
+    let result = await User.deleteOne(query);
 
     res.send(result).status(200);
 });
@@ -44,16 +43,40 @@ router.delete("/:id", async (req, res) => {
 // Update contact from user
 router.put("/:id/contact", async (req, res) => {
     const query = { _id: ObjectId(req.params.id) };
-    let user = await User.collection.findOne(query);
+    let user = await User.findOne(query);
     if (!user) {
         return res.send("User Not found!").status(404);
     }
-    req.body._id = ObjectId(user.contact._id)
     const updates = {
         $set: { contact: req.body }
     };
-    console.log(updates);
-    let result = await User.collection.updateOne(query, updates);
+    let result = await User.updateOne(query, updates);
+
+    res.send(result).status(200);
+});
+
+// Read all experiences
+router.get("/:id/experience", async (req, res) => {
+    const query = { _id: ObjectId(req.params.id) };
+    let user = await User.findOne(query);
+    if (!user) {
+        return res.send("User Not found!").status(404);
+    }
+
+    res.send(user.experiences).status(200);
+});
+
+// Add a new experience to the collection
+router.post("/:id/experience", async (req, res) => {
+    const query = { _id: ObjectId(req.params.id) };
+    let user = await User.findOne(query);
+    if (!user) {
+        return res.send("User Not found!").status(404);
+    }
+    const updates = {
+        $push: { experiences: req.body }
+    };
+    let result = await User.updateOne(query, updates);
 
     res.send(result).status(200);
 });
@@ -61,15 +84,37 @@ router.put("/:id/contact", async (req, res) => {
 // Update experience
 router.put("/:id/experience/:experience_id", async (req, res) => {
     const query = { _id: ObjectId(req.params.id) };
-    let user = await User.collection.findOne(query);
+    let user = await User.findOne(query);
     if (!user) {
         return res.send("User Not found!").status(404);
     }
     const updates = {
-        $set: { contact: req.body }
+        $set: {
+            'experiences.$.period': req.body.period,
+            'experiences.$.company': req.body.company,
+            'experiences.$.mission': req.body.mission,
+            'experiences.$.role': req.body.role,
+            'experiences.$.content': req.body.content,
+            'experiences.$.stack': req.body.stack
+        }
     };
-    console.log(updates);
-    let result = await User.collection.updateOne(query, updates);
+    let result = await User.updateOne({ 'experiences._id': ObjectId(req.params.experience_id) }, updates);
+
+    res.send(result).status(200);
+});
+
+// Delete an entry
+router.delete("/:id/experience/:experience_id", async (req, res) => {
+    const query = { _id: ObjectId(req.params.id) };
+    let user = await User.findOne(query);
+    if (!user) {
+        return res.send("User Not found!").status(404);
+    }
+    user.experiences.pull({ _id: ObjectId(req.params.experience_id) });
+    const updates = {
+        $set: { experiences: user.experiences }
+    }
+    let result = await User.updateOne(query, updates);
 
     res.send(result).status(200);
 });
